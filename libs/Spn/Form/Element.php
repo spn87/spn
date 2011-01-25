3ff303047f751bb936077478f23a90c2c7f4936e
Souphorn <ansouphorn@gmail.com>
2011-01-25 19:01:35 +0700

    Added dropdown and textarea control element of form

--- libs/Spn/Form/Element.php
+++ libs/Spn/Form/Element.php
@@ -9,11 +9,20 @@
 class Spn_Form_Element
 {
 	const REQUIRE_FIELD = 1;
+	const TEXT_AREA = 2;
+	const INPUT =3;
+	const DROP_DOWN_LIST = 4;
+	const D_DATA = 5;
 		
 	private $field;
 	private $label;
 	private $value = "";
 	private $isRequire = false;
+	private $controlType = self::INPUT;
+	private $getElFunc = "getElement";
+	
+	//Drop down list info
+	private $dData = array();
 	/**
 	 * 
 	 * Get the html form element
@@ -29,22 +38,76 @@ class Spn_Form_Element
 		
 		if (is_array($params))
 		{
-			foreach ($params as $p)
+			foreach ($params as $k=>$p)
 			{
-				if ($p == self::REQUIRE_FIELD)
-					$this->isRequire = true;
+				switch ($p)
+				{
+					case self::REQUIRE_FIELD:
+						$this->isRequire = true;
+						break;
+					case self::TEXT_AREA:
+						$this->controlType = self::TEXT_AREA;
+						$this->getElFunc = "getTextAreaElement";
+						break;
+					case self::DROP_DOWN_LIST:
+						$this->controlType = self::DROP_DOWN_LIST;
+						$this->getElFunc = "getDropDownList";
+						break;
+					
+				}
+				
+				switch($k)
+				{
+					case self::D_DATA:
+						$this->dData = $p;
+						break;
+				}
 			}
 		}
 	}
 	
 	public function getElement()
 	{
+		if ($this->controlType != self::INPUT) 
+		{
+			$func = $this->getElFunc;
+			return $this->$func();
+		}
 		$html ="
 			<div>
 				<label>".$this->label."</label>
 				<span><input type='text' name='".$this->field['Field']."' id='".$this->field['Field']."' class='".($this->isRequire ? "required":"")."' value='".$this->value."'/></span>
 			</div>
 			";
+		
+		return $html;
+	}
+	
+	private function getTextAreaElement()
+	{
+		$html ="
+			<div>
+				<label>".$this->label."</label>
+				<span><textarea  name='".$this->field['Field']."' id='".$this->field['Field']."' class='".($this->isRequire ? "required":"")."' cols='50' rows='7'>$this->value</textarea></span>
+			</div>
+			";
+		
+		return $html;
+	}
+	
+	private function getDropDownList()
+	{
+		$option = "";
+		
+		foreach ($this->dData as $k=>$v)
+		{
+			$option .= "<option value='$k'>".$v."</option>";
+		}
+		$html ="
+		<select name='".$this->field["Field"]."' id='".$this->field["Field"]."'>
+		$option
+		</select>
+		";
 		
 		return $html;
 	}
