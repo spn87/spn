029e28ef6c4b6d96a9cd83f39fad61ba4cd032fe
Souphorn <ansouphorn@gmail.com>
2011-01-26 17:45:53 +0700

    Added a function to get content related information

--- app/c/m/c_index.php
+++ app/c/m/c_index.php
@@ -11,6 +11,15 @@
 
 class CIndex_Model extends Spn_Model
 {
-	protected $_name = "contents";
+	protected $_name = "contents";	
+	public function baseUrlContent($url)
+	{
+		$query = "SELECT c.*,u.name AS creator FROM $this->_name AS c INNER JOIN ".Spn_User::$_table." AS u ON c.created_by=u.id WHERE url='".Spn_Db::e($url)."'";
+		//echo $query;
+		$rows = $this->_db->fetchAll($query);
+		if (count($rows)>0)
+			return $rows[0];
+		return null;
+	}
 }
 ?>
